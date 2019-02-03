25319c94d2bfd6fa027eebf1a22bef2ea0192aa2
Pamela <pamefer.95@gmail.com>
2019-02-02 22:44:19 -0500

    2.3 Add test to get library number

---
+++ test/com/twu/biblioteca/UserTest.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+    User user;
+
+    @Before
+    public void chargeUserData(){
+         user = new User( "000-0001", "pass123");
+    }
+
+    @Test
+    public void shouldReturnLibraryNumber(){
+        String expected = "000-0001";
+        String actual = user.getLibraryNumber();
+        assertEquals(expected, actual);
+    }
+}
