c9f0d06943394727538d330de038b5af73095d52
Pamela <pamefer.95@gmail.com>
2019-02-04 14:05:06 -0500

    2.3 add test to Reservation

---
+++ test/com/twu/biblioteca/ReservationTest.java
@@ -0,0 +1,30 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ReservationTest {
+    Reservation reservation;
+    User user;
+    Book book;
+
+    @Before
+    public void chargeReservationData(){
+        user = new User( "000-0001", "1234", false);
+        book = new Book("Flowers", true, "1678", "Radnedy Cajas");
+        reservation = new Reservation(user, book);
+    }
+
+    @Test
+    public void shouldReturnUserObject(){
+        assertTrue(reservation.getUser() instanceof User);
+    }
+
+    @Test
+    public void shouldReturnBookObject(){
+        assertTrue(reservation.getBook() instanceof Book);
+    }
+}
