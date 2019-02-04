f1e5e3c8f3687d81ae949d0d8e8370510f994c0c
Pamela <pamefer.95@gmail.com>
2019-02-04 00:48:00 -0500

    2.3 Fix refactor another tests

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,18 +13,25 @@ public class BibliotecaTest {
     List<Book> listBooks = new ArrayList<>();
     private final int BOOK_OPTION = 1;
     private final int MOVIE_OPTION = 2;
+    private UserObjects userObject;
+
+    @Before
+    public void chargeUserObject(){
+        userObject = new UserObjects();
+        userObject.login("1","1234");
+    }
 
     @Test
     public void shouldReturnWelcomeMessage() {
         String expected = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore";
-        String actual = new Biblioteca().welcomeMessage();
+        String actual = new Biblioteca(userObject).welcomeMessage();
         assertEquals(expected, actual);
     }
 
     /* Check out book */
     @Test
     public void shouldChekoutABook(){
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         boolean isCheckoutActual = biblioteca.checkoutResource("You", BOOK_OPTION);
 
@@ -32,7 +40,7 @@ public class BibliotecaTest {
 
     @Test
     public void shouldNotChekoutABook(){
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         boolean isCheckoutActual = biblioteca.checkoutResource("dgadah" , BOOK_OPTION);
 
@@ -42,7 +50,7 @@ public class BibliotecaTest {
     @Test
     public void shouldBeOneBookLessFromAllBooksIfCheckOut(){
         int numberBookExpected = 2;
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         biblioteca.checkoutResource("You", BOOK_OPTION);
         int numberBooksActual = biblioteca.getListResourceAvailable(1).size();
@@ -53,7 +61,7 @@ public class BibliotecaTest {
     @Test
     public void shouldNotBeOneBookLessFromAvailableBooksIfBookNotExist(){
         int numberBookExpected = 3;
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         biblioteca.checkoutResource("Me", BOOK_OPTION);
         int numberBooksActual = biblioteca.getListResourceAvailable(1).size();
@@ -65,7 +73,7 @@ public class BibliotecaTest {
         String successMessageExpected = "Thank you, enjoy the book";
         Boolean isCheckOut = true;
 
-        String successMessageActual = new Biblioteca().returnMessageCheckout(isCheckOut);
+        String successMessageActual = new Biblioteca(userObject).returnMessageCheckout(isCheckOut);
 
         assertEquals( successMessageExpected, successMessageActual);
     }
@@ -75,7 +83,7 @@ public class BibliotecaTest {
         String unsuccessMessageExpected = "Sorry, that book is not available";
         Boolean isCheckOut = false;
 
-        String unsuccessMessageActual = new Biblioteca().returnMessageCheckout(isCheckOut);
+        String unsuccessMessageActual = new Biblioteca(userObject).returnMessageCheckout(isCheckOut);
 
         assertEquals( unsuccessMessageExpected, unsuccessMessageActual);
     }
@@ -83,19 +91,19 @@ public class BibliotecaTest {
     /* Return Book */
 
     /* ask ??*/
-    @Test
-    public void shouldReturnABook(){
-        Book b = new Book("You", false, "Pamela", "2015");
-        Biblioteca biblioteca = new Biblioteca();
-
-        boolean isReturnActual = biblioteca.returnBook(b.getName());
-
-        assertTrue(isReturnActual);
-    }
+//    @Test
+//    public void shouldReturnABook(){
+//        Book b = new Book("You", false, "Pamela", "2015");
+//        Biblioteca biblioteca = new Biblioteca(shouldReturnSuccessMessageIfCheckOutTrue(););
+//
+//        boolean isReturnActual = biblioteca.returnBook(b.getName());
+//
+//        assertTrue(isReturnActual);
+//    }
 
     @Test
     public void shouldNotReturnABook(){
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         boolean isReturnActual = biblioteca.returnBook("Adasdasd");
 
@@ -105,7 +113,7 @@ public class BibliotecaTest {
     @Test
     public void shouldBeOneBookMoreFromAvailableBooksIfIsReturned(){
         int numberBookExpected = 3;
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         biblioteca.returnBook("You");
         int numberBooksActual = biblioteca.getListResourceAvailable(1).size();
@@ -117,7 +125,7 @@ public class BibliotecaTest {
         String successMessageExpected = "Thank you for returning the book";
         Boolean isReturned = true;
 
-        String successMessageActual = new Biblioteca().returnMessageReturn(isReturned);
+        String successMessageActual = new Biblioteca(userObject).returnMessageReturn(isReturned);
 
         assertEquals( successMessageExpected, successMessageActual);
     }
@@ -127,7 +135,7 @@ public class BibliotecaTest {
         String successMessageExpected = "That is not a valid book to return";
         Boolean isReturned = false;
 
-        String successMessageActual = new Biblioteca().returnMessageReturn(isReturned);
+        String successMessageActual = new Biblioteca(userObject).returnMessageReturn(isReturned);
 
         assertEquals( successMessageExpected, successMessageActual);
     }
@@ -137,7 +145,7 @@ public class BibliotecaTest {
     public void shouldReturnTrueIfIsInstanceOfBook(){
         Resource book = new Book("Book1", true, "2016", "Autor");
 
-        Boolean actual = new Biblioteca().isInstanceOfBook(book);
+        Boolean actual = new Biblioteca(userObject).isInstanceOfBook(book);
 
         assertTrue(actual);
     }
@@ -146,7 +154,7 @@ public class BibliotecaTest {
     public void shouldReturnTrueIfIsInstanceOfMovie(){
         Resource movie = new Movie("Movie1", true, "1678", "Director", "5");
 
-        Boolean actual = new Biblioteca().isInstanceOfMovie(movie);
+        Boolean actual = new Biblioteca(userObject).isInstanceOfMovie(movie);
 
         assertTrue(actual);
     }
@@ -155,7 +163,7 @@ public class BibliotecaTest {
 
     @Test
     public void shouldNotChekoutAMovie(){
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         boolean isCheckoutActual = biblioteca.checkoutResource("MovieX", MOVIE_OPTION);
 
@@ -165,7 +173,7 @@ public class BibliotecaTest {
     @Test
     public void shouldBeOneMovieLessFromAllMoviesIfCheckOut(){
         int numberMoviesExpected = 2;
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         biblioteca.checkoutResource("Movie1", MOVIE_OPTION);
         int numberMoviesActual = biblioteca.getListResourceAvailable(MOVIE_OPTION).size();
@@ -176,7 +184,7 @@ public class BibliotecaTest {
     @Test
     public void shouldNotBeOneMovieLessFromAvailableMoviesIfMovieNotExist(){
         int numberMoviesExpected = 3;
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
 
         biblioteca.checkoutResource("MovieX", MOVIE_OPTION);
         int numberMoviesActual = biblioteca.getListResourceAvailable(MOVIE_OPTION).size();
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,14 +1,22 @@
 package com.twu.biblioteca;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class MenuTest {
+    private UserObjects userObject;
+    @Before
+    public void chargeUserObject(){
+        userObject = new UserObjects();
+        userObject.login("1","1234");
+    }
+
     @Test
     public void shouldReturnValidOptionMenu(){
         Boolean expected = true;
-        Biblioteca biblioteca= new Biblioteca();
+        Biblioteca biblioteca= new Biblioteca(userObject);
         Menu menu = new Menu(biblioteca);
         Boolean actual = new Utililty().isValidOption("2", menu.getOptionsList());
         assertEquals(expected, actual);
@@ -17,7 +25,7 @@ public class MenuTest {
     @Test
     public void shouldReturnInvalidOptionMenuOutOfRange(){
         Boolean expected = false;
-        Biblioteca biblioteca= new Biblioteca();
+        Biblioteca biblioteca= new Biblioteca(userObject);
         Menu menu = new Menu(biblioteca);
         Boolean actual = new Utililty().isValidOption("8", menu.getOptionsList());
         assertEquals(expected, actual);
@@ -26,7 +34,7 @@ public class MenuTest {
     @Test
     public void shouldReturnInvalidOptionMenuNotNumber(){
         Boolean expected = false;
-        Biblioteca biblioteca= new Biblioteca();
+        Biblioteca biblioteca= new Biblioteca(userObject);
         Menu menu = new Menu(biblioteca);
         Boolean actual = new Utililty().isValidOption("jjjjj", menu.getOptionsList());
         assertEquals(expected, actual);
@@ -34,7 +42,7 @@ public class MenuTest {
 
     @Test
     public void shouldReturnFalseIfNotSelectOptionOfQuit() {
-        Biblioteca biblioteca = new Biblioteca();
+        Biblioteca biblioteca = new Biblioteca(userObject);
         Boolean actual = new Menu(biblioteca).isOptionToQuit(6);
         assertFalse(actual);
     }
