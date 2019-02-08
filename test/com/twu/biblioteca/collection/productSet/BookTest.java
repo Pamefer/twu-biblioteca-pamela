9c97443d24129aa69040c061c0d39f131ba145da
Pamela <pamefer.95@gmail.com>
2019-02-08 11:49:59 -0500

    Add extra test in book and movie

--- test/com/twu/biblioteca/collection/productSet/BookTest.java
+++ test/com/twu/biblioteca/collection/productSet/BookTest.java
@@ -2,8 +2,10 @@ package com.twu.biblioteca.collection.productSet;
 import com.twu.biblioteca.collection.productSet.Book;
 import com.twu.biblioteca.collection.userSet.User;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BookTest {
     private Book book;
@@ -13,4 +15,26 @@ public class BookTest {
         User user = new User();
         book = new Book("Flowers", true, user, "1678", "Radnedy Cajas");
     }
+
+    @Test
+    public void shouldGetNameOfBook() {
+        String actual = book.getName();
+        assertEquals("Flowers", actual);
+    }
+
+    @Test
+    public void shouldGetAvailabilityOfBook(){
+        Boolean actual = book.getAvailable();
+        assertTrue(actual);
+    }
+
+    @Test
+    public void shouldGetYearOfBook(){
+        String name = "Flowers";
+        String author = "Radnedy Cajas";
+        String year = "1678";
+        String bookCast = book.toString();
+        assertTrue(bookCast.contains(name) && bookCast.contains(author) && bookCast.contains(year));
+    }
+
 }
--- test/com/twu/biblioteca/collection/productSet/MovieTest.java
+++ test/com/twu/biblioteca/collection/productSet/MovieTest.java
@@ -3,27 +3,38 @@ package com.twu.biblioteca.collection.productSet;
 import com.twu.biblioteca.collection.productSet.Movie;
 import com.twu.biblioteca.collection.userSet.User;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MovieTest {
     Movie movie;
 
     @Before
-    public void chargeMovie(){
+    public void chargeMovie() {
         User user = new User();
         movie = new Movie("Sky", true, user, "2016", "Adam Sandler", "3");
     }
 
-//    @Test
-//    public void shouldReturnDirector(){
-//        String actual = movie.getDirector();
-//        assertEquals("Adam Sandler", actual);
-//    }
-//
-//    @Test
-//    public void shouldReturnRating(){
-//        String actual = movie.getRating();
-//        assertEquals("3", actual);
-//    }
+    @Test
+    public void shouldGetNameOfMovie() {
+        String actual = movie.getName();
+        assertEquals("Sky", actual);
+    }
+
+    @Test
+    public void shouldGetAvailabilityOfMovie() {
+        Boolean actual = movie.getAvailable();
+        assertTrue(actual);
+    }
+
+    @Test
+    public void shouldGetYearOfBook() {
+        String name = "Sky";
+        String director = "Adam Sandler";
+        String year = "2016";
+        String movieCast = movie.toString();
+        assertTrue(movieCast.contains(name) && movieCast.contains(director) && movieCast.contains(year));
+    }
 }
